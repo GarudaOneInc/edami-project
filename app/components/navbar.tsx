cd0173650811b5af518d105945f4ca386944a9a9
Anand Loorthu Raj A <garudaoneinc@gmail.com>
2024-12-03 01:08:17 +0530

    shadcn_ui + github_avatar + navbar changes made

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import Link from "next/link";
 import Image from 'next/image';
-import { auth, signOut, signIn  } from '@/auth';
+import { auth, signOut, signIn } from '@/auth';
+import * as Avatar from "@radix-ui/react-avatar";
+
 
 const Navbar = async () => {
   const session = await auth();
 
   return (
-    <header className='px-5 py-3 bg-white shadow-sm max-w-screen-xl mx-auto rounded-b-2xl'>
+    <header className='px-5 py-2 bg-white shadow-sm max-w-screen-lg mx-auto rounded-b-2xl'>
       <nav className='flex justify-between items-center'>
         <Link href="/">
-          <Image src="./logo.svg" alt='logo' width={100} height={22}/>
+          <Image src="./logo.svg" alt='logo' className='py-2' width={120} height={30}/>
         </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          <Link href="/explore">Explore</Link>
+          <Link href="/pricing">Pricing</Link>
+          <Link href="/resource">Resource</Link>
+        </div>
 
         <div className="flex items-center gap-5 text-black">
           {session && session?.user ? (
@@ -33,7 +41,9 @@ const Navbar = async () => {
               </form>
 
               <Link href={`/user/${session?.user?.id}`}>
-                <span>{session?.user?.name}</span>
+                <Avatar.Root>
+                  <Avatar.Image className='Github_Avatar rounded-full' src={session?.user?.image || ""} width={40} />
+                </Avatar.Root>
               </Link>
             </>
           ) : (
